d4fdc2c6156c6a35ea9020b5497d667f2a7f0f04
Félix VEYSSEYRE <felix.veysseyre@gmail.com>
2015-08-04 11:42:19 +0200

    Implement SendNotification command

---
+++ src/LogAnalyzer/CoreBundle/Command/SendNotificationCommand.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace LogAnalyzer\CoreBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SendNotificationCommand extends ContainerAwareCommand
+{
+	/* Protected */
+
+	protected function configure()
+	{
+		$this
+			-> setName('logAnalyzer:notificationAdministration:sendNotification')
+			-> setDescription('Send notification');
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$startTime = microtime(true);
+
+		/* Get return value */
+
+		$returnValue = $this -> sendNotification();
+
+		$executionTime = $this -> getContainer() -> get('Helpers') -> getExecutionTime($startTime, microtime(true));
+
+		/* Prepare return object */
+
+		if($returnValue === true)
+		{
+			$data = array(
+				'resultCode' => 1,
+				'executionTime' => $executionTime,
+				'message' => 'Notifications have been sent.'
+			);
+		}
+		else
+		{
+			$data = array(
+				'resultCode' => -1,
+				'executionTime' => $executionTime,
+				'message' => 'Notification sending has failed.'
+			);
+		}
+
+		$output -> writeln(json_encode($data));
+	}
+
+	/* Private */
+
+	private function sendNotification()
+	{
+		$notificationIdsToDelete = array();
+
+		/* Get notifications to send */
+
+		$notificationsToSend = $this
+			-> getNotificationToSendRepository()
+			-> getNotificationToSend();
+
+		/* Send notifications */
+
+		foreach($notificationsToSend as $notificationToSend)
+		{
+			if($notificationToSend -> getType() === 'email')
+			{
+				foreach($notificationToSend -> getRecipient() as $email)
+				{
+					$content = $notificationToSend -> getContent();
+
+					$return = $this -> sendEmail($email, $content['subject'], $content['message']);
+
+					if($return)
+					{
+						array_push($notificationIdsToDelete, $notificationToSend -> getNotificationToSendId());
+					}
+				}
+			}
+			elseif($notificationToSend -> getType() === 'collector')
+			{
+				foreach($notificationToSend -> getRecipient() as $collectorHuman)
+				{
+					$collectors = $this
+						-> getCollectorRepository()
+						-> getCollector(array('collectorHuman' => $collectorHuman));
+
+					if(is_array($collectors) && sizeof($collectors) === 1)
+					{
+						$IP = $collectors[0] -> getIP();
+						$port = $collectors[0] -> getPort();
+
+						$content = $notificationToSend -> getContent();
+
+						$return = $this -> sendMessageToCollector($IP, $port, $content['message']);
+
+						if($return)
+						{
+							array_push($notificationIdsToDelete, $notificationToSend -> getNotificationToSendId());
+						}
+					}
+				}
+			}
+		}
+
+		/* Clean notification */
+
+		foreach($notificationIdsToDelete as $notificationIdToDelete)
+		{
+			$this
+				-> getNotificationToSendRepository()
+				-> deleteNotificationToSend(array('notificationToSendId' => $notificationIdToDelete));
+		}
+
+		/* Return */
+
+		return true;
+	}
+
+	/* Private */
+
+	private function sendEmail($email, $subject, $message)
+	{
+		$emailNotifier = $this
+			-> getContainer()
+			-> get('EmailNotifier');
+
+		return $emailNotifier -> sendMessage($email, $subject, $message);
+	}
+
+	private function sendMessageToCollector($IP, $port, $message)
+	{
+		$collectorNotifier = $this
+			-> getContainer()
+			-> get('CollectorNotifier');
+
+		return $collectorNotifier -> sendMessage($IP, $port, $message);
+	}
+
+	/* Special */
+
+	private function getNotificationToSendRepository()
+	{
+		return $this
+			-> getContainer()
+			-> get('doctrine_mongodb')
+			-> getManager()
+			-> getRepository('LogAnalyzerCoreBundle:NotificationToSend');
+	}
+
+	private function getCollectorRepository()
+	{
+		return $this
+			-> getContainer()
+			-> get('doctrine_mongodb')
+			-> getManager()
+			-> getRepository('LogAnalyzerCoreBundle:Collector');
+	}
+}
