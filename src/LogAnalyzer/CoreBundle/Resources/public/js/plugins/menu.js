760b75f0a8392749bbbf89bd471db33bf103e143
Félix VEYSSEYRE <felix.veysseyre@gmail.com>
2015-08-07 13:13:28 +0200

    Clean Menu plugin

--- src/LogAnalyzer/CoreBundle/Resources/public/js/plugins/menu.js
+++ src/LogAnalyzer/CoreBundle/Resources/public/js/plugins/menu.js
@@ -36,151 +36,120 @@ $(function($){
 
 			if(sections)
 			{
-				var ulStructure = '<ul id="#id#" class="#class#">#content#</ul>'; //class = menu || subMenu
-				var liStructure = '<li id="#id#" class="#class#"><span class="container #spanClass#">#content#</span></li>'; //class = section || subSection
+				var ulStructure = '<ul id="#id#" class="#class#"></ul>';
+				var ulElement, subUlElement;
+
+				var liStructure =
+					'<li id="#id#" class="#class#">' +
+						'<span class="container #spanClass#">' +
+							'#content#' +
+						'</span>' +
+					'</li>';
+				var liElement, subLiElement;
+
 				var contentStructure = '#icon#<span class="name">#name#</span>';
+				var contentTemp;
 
 				/* Create the main menu container */
 
-				this.element
-					.append(ulStructure
-						.replace(/#id#/, '')
-						.replace(/#class#/, 'menuContainer menu')
-						.replace(/#content#/, '')
+				ulElement = $(ulStructure
+					.replace(/#id#/, '')
+					.replace(/#class#/, 'menuContainer menu')
 				);
 
-				var menuContainer = this.element.find('.menuContainer');
-
 				/* Create sections and subSections */
-
-				var contentTemp;
-				var spanClassTemp;
 
 				for(var i = 0; i < sections.length; i++)
 				{
 					var sectionTemp = sections[i];
 
-					/* Text */
+					/* Section structure */
 
 					contentTemp = contentStructure
 						.replace(/#icon#/, (sectionTemp.icon) ? '<i class="fa ' + sectionTemp.icon + '"></i>' : '')
 						.replace(/#name#/, sectionTemp.name);
 
-					/* Activation */
-
-					spanClassTemp = (! sectionTemp.onActivation) ? 'disabled' : '';
-
-					/* Section structure */
-
-					menuContainer.append(liStructure
+					liElement = $(liStructure
 						.replace(/#id#/, sectionTemp.id)
 						.replace(/#class#/, 'section')
-						.replace(/#spanClass#/, spanClassTemp)
+						.replace(/#spanClass#/, (! sectionTemp.onActivation) ? 'disabled' : '')
 						.replace(/#content#/, contentTemp)
 					);
 
-					var sectionContainerTemp = this.element.find('#' + sectionTemp.id);
-
-					/* Section logic */
-
-					/* Logic */
-
-					var self = this;
-
-					if(sectionTemp.onActivation)
-					{
-						(function(){
-							var callbackOnActivationMainTemp = self.getOption('onActivation') || function(){};
-							var callbackOnDeactivationMainTemp = self.getOption('onDeactivation') || function(){};
-
-							var callbackOnActivationTemp = sectionTemp.onActivation || function(){};
-							var callbackOnDeactivationTemp = sectionTemp.onDeactivation || function(){};
-
-							sectionContainerTemp.find('.container').click(function()
-							{
-								if($(this).hasClass('active'))
-								{
-									$(this).activeMenu(false);
-									callbackOnDeactivationTemp();
-									callbackOnDeactivationMainTemp();
-								}
-								else
-								{
-									$(this).activeMenu(true);
-									callbackOnActivationMainTemp();
-									callbackOnActivationTemp();
-								}
-							});
-						})();
-					}
+					this._addCallbackFunction(sectionTemp, liElement);
 
 					if(sectionTemp.subSections)
 					{
-						sectionContainerTemp
-							.append(ulStructure
-								.replace(/#id#/, sectionTemp.id + 'SubMenuContainer')
-								.replace(/#class#/, 'subMenu')
-								.replace(/#content#/, '')
+						subUlElement = $(ulStructure
+							.replace(/#id#/, sectionTemp.id + 'SubMenuContainer')
+							.replace(/#class#/, 'subMenu')
 						);
-
-						var subMenuContainerTemp = this.element.find('#' + sectionTemp.id + 'SubMenuContainer');
 
 						for(var j = 0; j < sectionTemp.subSections.length; j++)
 						{
 							var subSectionTemp = sectionTemp.subSections[j];
 
-							/* Icon */
+							/* SubSection structure */
 
 							contentTemp = contentStructure
 								.replace(/#icon#/, (subSectionTemp.icon) ? '<i class="fa ' + subSectionTemp.icon + '"></i>' : '')
 								.replace(/#name#/, subSectionTemp.name);
 
-							/* Activation */
-
-							spanClassTemp = (! subSectionTemp.onActivation) ? 'disabled' : '';
-
-							/* SubSection structure */
-
-							subMenuContainerTemp
-								.append(liStructure
-									.replace(/#id#/, subSectionTemp.id)
-									.replace(/#class#/, 'subSection')
-									.replace(/#spanClass#/, spanClassTemp)
-									.replace(/#content#/, contentTemp)
+							subLiElement = $(liStructure
+								.replace(/#id#/, subSectionTemp.id)
+								.replace(/#class#/, 'subSection')
+								.replace(/#spanClass#/, (! subSectionTemp.onActivation) ? 'disabled' : '')
+								.replace(/#content#/, contentTemp)
 							);
 
-							var subSectionContainerTemp = this.element.find('#' + subSectionTemp.id);
+							this._addCallbackFunction(subSectionTemp, subLiElement);
 
-							/* Logic */
+							/* Append */
 
-							if(subSectionTemp.onActivation)
-							{
-								(function(){
-									var callbackOnActivationMainTemp = self.getOption('onActivation') || function(){};
-									var callbackOnDeactivationMainTemp = self.getOption('onDeactivation') || function(){};
-
-									var callbackOnActivationTemp = subSectionTemp.onActivation || function(){};
-									var callbackOnDeactivationTemp = subSectionTemp.onDeactivation || function(){};
-
-									subSectionContainerTemp.find('.container').click(function(){
-										if($(this).hasClass('active'))
-										{
-											$(this).activeMenu(false);
-											callbackOnDeactivationTemp();
-											callbackOnDeactivationMainTemp();
-										}
-										else
-										{
-											$(this).activeMenu(true);
-											callbackOnActivationMainTemp();
-											callbackOnActivationTemp();
-										}
-									});
-								})();
-							}
+							subUlElement.append(subLiElement);
 						}
+
+						/* Append */
+
+						liElement.append(subUlElement);
 					}
+
+					/* Append */
+
+					ulElement.append(liElement);
 				}
+
+				/* Append */
+
+				this.element.append(ulElement);
+			}
+		},
+
+		_addCallbackFunction: function(section, liElement)
+		{
+			if(section.onActivation)
+			{
+				var callbackOnActivationMainTemp = this.getOption('onActivation') || function(){};
+				var callbackOnDeactivationMainTemp = this.getOption('onDeactivation') || function(){};
+
+				var callbackOnActivationTemp = section.onActivation || function(){};
+				var callbackOnDeactivationTemp = section.onDeactivation || function(){};
+
+				liElement.find('.container').click(function()
+				{
+					if($(this).hasClass('active'))
+					{
+						$(this).activeMenu(false);
+						callbackOnDeactivationTemp();
+						callbackOnDeactivationMainTemp();
+					}
+					else
+					{
+						$(this).activeMenu(true);
+						callbackOnActivationMainTemp();
+						callbackOnActivationTemp();
+					}
+				});
 			}
 		},
 
